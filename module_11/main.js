f645ccefd65dfc1c9ef97bf86b3c788c489282bc
vladSHOKO <vladislav_kuzminov.00@mail.ru>
2024-08-20 10:43:14 +0300

    Complete module 11 with additional task

--- module_11/main.js
+++ module_11/main.js
@@ -15,31 +15,116 @@ function createFindButtonListener() {
     let findButton = document.getElementById("search_request_btn")
     findButton.addEventListener("click", function () {
         document.getElementById("search_result_list").innerHTML = ""
-        let inputText = document.getElementById("person_search_input").value
-        let api = "https://swapi.dev/api/";
-        let url = api + "people/?search=" + inputText;
-        fetch(url).then(response => response.json()).then(result => {
-            showResults(result)
-        })
+        swapiRequest()
     })
+}
+
+async function swapiRequest() {
+    let inputText = document.getElementById("person_search_input").value
+    let searchType = document.getElementById("select-list").value
+    changePersonData()
+
+    let api = "https://swapi.dev/api/";
+    let url = api + searchType + "/?search=" + inputText;
+
+    let response = await fetch(url)
+    if (response.ok) {
+        let result = await response.json();
+        await showResults(result)
+    } else {
+        alert("HTTP error " + response.status)
+    }
+}
+
+function changePersonData() {
+    let div = document.getElementById("person-data")
+    let searchType = document.getElementById("select-list").value
+    if (searchType === "people") {
+        div.innerHTML = '<div>Имя: <span id="name"></div>\n' +
+            '        <div>Рост: <span id="height"></div>\n' +
+            '        <div>Вес: <span id="mass"></div>\n' +
+            '        <div>Год рождения: <span id="birth_year"></div>\n' +
+            '        <div>В скольки фильмах появлялся: <span id="films_count"></div>'
+    } else if (searchType === "starships") {
+        div.innerHTML = '<div>Имя: <span id="name"></div>\n' +
+            '        <div>Модель: <span id="model"></div>\n' +
+            '        <div>Количество пассажиров: <span id="passengers"></div>\n' +
+            '        <div>Класс корабля: <span id="starship_class"></div>\n' +
+            '        <div>В скольки фильмах появлялся: <span id="films_count"></div>'
+    } else {
+        div.innerHTML = '<div>Имя: <span id="name"></div>\n' +
+            '        <div>Климат: <span id="climate"></div>\n' +
+            '        <div>Диаметр: <span id="diameter"></div>\n' +
+            '        <div>Популяция: <span id="population"></div>\n' +
+            '        <div>В скольки фильмах появлялся: <span id="films_count"></div>'
+    }
+
+
+}
+
+function showPeopleData(result) {
+    let name = document.getElementById("name")
+    name.textContent = result.name
+
+    let height = document.getElementById("height")
+    height.textContent = result.height
+
+    let mass = document.getElementById("mass")
+    mass.textContent = result.mass
+
+    let birthYear = document.getElementById("birth_year")
+    birthYear.textContent = result.birth_year
+
+    let filmsCount = document.getElementById("films_count")
+    filmsCount.textContent = result.films.length
+}
+
+function showStarshipsData(result) {
+    let name = document.getElementById("name")
+    name.textContent = result.name
+
+    let model = document.getElementById("model")
+    model.textContent = result.model
+
+    let passengers = document.getElementById("passengers")
+    passengers.textContent = result.passengers
+
+    let starshipClass = document.getElementById("starship_class")
+    starshipClass.textContent = result.starship_class
+
+    let filmsCount = document.getElementById("films_count")
+    filmsCount.textContent = result.films.length
+}
+
+function showPlanetsData(result) {
+    let name = document.getElementById("name")
+    name.textContent = result.name
+
+    let climate = document.getElementById("climate")
+    climate.textContent = result.climate
+
+    let diameter = document.getElementById("diameter")
+    diameter.textContent = result.diameter
+
+    let population = document.getElementById("population")
+    population.textContent = result.population
+
+    let filmsCount = document.getElementById("films_count")
+    filmsCount.textContent = result.films.length
+
 }
 
 function createResultListener(result, element) {
     element.addEventListener("click", function () {
-        let name = document.getElementById("name")
-        name.textContent = result.name
+        let searchType = document.getElementById("select-list").value
+        if (searchType === "people") {
+            showPeopleData(result)
+        } else if (searchType === "starships") {
+            showStarshipsData(result)
+        } else {
+            showPlanetsData(result)
+        }
 
-        let height = document.getElementById("height")
-        height.textContent = result.height
-
-        let mass = document.getElementById("mass")
-        mass.textContent = result.mass
-
-        let birthYear = document.getElementById("birth_year")
-        birthYear.textContent = result.birth_year
-
-        let filmsCount = document.getElementById("films_count")
-        filmsCount.textContent = result.films.length
     })
 }
 
